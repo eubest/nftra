6b8ff17f751cecf40f2bd3b46ece8a561acc5cab
eubest <eubest00@gmail.com>
2023-09-29 22:03:59 +0300

    async events

--- apps/saleor-app-checkout/pages/api/webhooks/saleor/transaction-charge-requested.ts
+++ apps/saleor-app-checkout/pages/api/webhooks/saleor/transaction-charge-requested.ts
@@ -30,7 +30,7 @@ export const TransactionChargeRequestedWebhook =
   new SaleorAsyncWebhook<TransactionActionPayloadFragment>({
     name: "Checkout app payment notifications",
     webhookPath: "api/webhooks/saleor/transaction-charge-requested",
-    syncEvent: "TRANSACTION_CHARGE_REQUESTED",
+    asyncEvent: "TRANSACTION_CHARGE_REQUESTED",
     apl: saleorApp.apl,
     subscriptionQueryAst: TransactionChargeRequestedSubscriptionDocument,
   });
